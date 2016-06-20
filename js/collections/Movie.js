a96e705e111a008cd69089f3a19882f259b5ebe7
DataNick <DataNick@users.noreply.github.com>
2016-06-20 04:54:58 -0400

    Added getMovies, renderMovies and updated render function with a template

--- js/collections/Movie.js
+++
@@ -1,5 +0,0 @@
-var Movies = Backbone.Collection.extend({
-  initialize: function(){
-
-  }
-});
--- js/views/search.js
+++ js/views/search.js
@@ -1,10 +1,35 @@
 MovieApp.Views.Search = Backbone.View.extend({
 
-    initialize: function(options) {},
+  template: "<input type = 'text' placeholder = 'search'> \
+            <button>Search Movie</button> \
+            <ul id = 'movie-list'></ul>",
 
-    render: function()
-    {
-        this.$el.html("This is my search page!!");
-        return this;
+  initialize: function(options) {},
+
+  render: function()
+  {
+      this.$el.html(this.template);
+      return this;
+  },
+
+  getMovies: function(){
+
+    var title = this.$el.find('input').val();
+    var movies = new MovieApp.Collections.Movies({title: title});
+
+    movies.fetch({success: this.renderMovies.bind(this)});
+  },
+
+  renderMovies: function(movies){
+    var movieview;
+
+    for (var n in movies.models) {
+      movieview = new MovieApp.Views.MovieView({model: movies.models[n]});
+      this.$el.find('#movie-list').append(movieview.render().el);
     }
+  },
+
+  events: {
+    'click button' : 'getMovies'
+  }
 });
