206c77106cdf1884d28400de67ba22982e084747
DataNick <DataNick@users.noreply.github.com>
2016-06-20 04:53:30 -0400

    Added url and parse functions

---
+++ js/collections/Movies.js
@@ -0,0 +1,14 @@
+MovieApp.Collections.Movies = Backbone.Collection.extend({
+  initialize: function(options){
+    if (options.title)
+      this.title = options.title;
+  },
+
+  url: function(){
+    return "http://www.omdbapi.com/?t=" + this.title;
+  },
+
+  parse: function(response){
+    return response.Search;
+  }
+});
