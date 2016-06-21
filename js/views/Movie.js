bfc7790630f505601f163b6e207420f287167805
DataNick <DataNick@users.noreply.github.com>
2016-06-21 02:52:35 -0400

    Added styling to titles

--- js/views/Movie.js
+++ js/views/Movie.js
@@ -9,7 +9,7 @@ MovieApp.Views.MovieView = Backbone.View.extend({
   },
 
   render: function(){
-    this.$el.html("<h1>" + this.model.attributes.Title + "</h1>" + "<p>"+this.model.attributes.Year+"</p>" + "<div class = 'plot'>" + this.model.attributes.Plot) + "</div>" + this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
+    this.$el.html("<h1>" + this.model.attributes.Title + "</h1>" + "<h5>Released: "+this.model.attributes.Year+"</h5>" + "<div class = 'plot'>" + this.model.attributes.Plot) + "</div>" + this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
     // this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
     return this;
   }
