3dfe1468fde2de8c32b25c5eedd74b76cffe4f7f
DataNick <DataNick@users.noreply.github.com>
2016-06-20 20:48:01 -0400

    Debugging issues

--- js/models/Movie.js
+++ js/models/Movie.js
@@ -1,6 +1,17 @@
 MovieApp.Models.Movies = Backbone.Model.extend({
 
-  initialize: function(options) {
+ initialize: function(options){
+   if (options.title)
+     this.title = options.title;
+ },
 
-  }
+ url: function(){
+   return "http://www.omdbapi.com/?t=" + this.title;
+ },
+
+ parse: function(response){
+  // console.log(response.Title)
+   return response;
+ }
+
 });
