9a712f44cbf6f4729868c73b2a7498f3b8b51728
DataNick <DataNick@users.noreply.github.com>
2016-06-20 20:49:30 -0400

    Added functionality to click enter to submit query and to prevent empty search form to send GET request

--- js/views/search.js
+++ js/views/search.js
@@ -1,8 +1,9 @@
 MovieApp.Views.Search = Backbone.View.extend({
 
-  template: "<input type = 'text' placeholder = 'search'> \
+
+  template: "<input id = 'new-search' type = 'text' placeholder = 'search'> \
             <button>Search Movie</button> \
-            <ul id = 'movie-list'></ul>",
+            <div id = 'movie-list'></div>",
 
   initialize: function(options) {},
 
@@ -13,23 +14,41 @@ MovieApp.Views.Search = Backbone.View.extend({
   },
 
   getMovies: function(){
-
-    var title = this.$el.find('input').val();
-    var movies = new MovieApp.Collections.Movies({title: title});
-
+    if (!this.$el.find('#new-search').val().trim()){
+      return;
+    }
+    else {
+      var title = this.$el.find('input').val();
+    }
+    // var movies = new MovieApp.Collections.Movies({title: title});
+    var movies = new MovieApp.Models.Movies({title: title})
     movies.fetch({success: this.renderMovies.bind(this)});
+
+    // var poster = $.ajax {
+    //   url: this.model.attributes.Poster;
+    // }
+  },
+
+  newSearch: function(e){
+    if (e.which !== 13 || !this.$el.find('#new-search').val().trim()){
+      return;
+    }
+    this.getMovies();
+
   },
 
   renderMovies: function(movies){
+    console.log('renderMovies hit')
+    console.log(this.input)
     var movieview;
 
-    for (var n in movies.models) {
-      movieview = new MovieApp.Views.MovieView({model: movies.models[n]});
-      this.$el.find('#movie-list').append(movieview.render().el);
-    }
+    movieview = new MovieApp.Views.MovieView({model: movies});
+    this.$el.find('#movie-list').html(movieview.render().el);
+
   },
 
   events: {
-    'click button' : 'getMovies'
+    'click button' : 'getMovies',
+    'keypress #new-search' : 'newSearch'
   }
 });
