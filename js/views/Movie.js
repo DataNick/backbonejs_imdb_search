be30a39413e10bd062f7bdc647604b6bcb4724e4
DataNick <DataNick@users.noreply.github.com>
2016-06-20 21:36:17 -0400

    Appending img element to show movie poster

--- js/views/Movie.js
+++ js/views/Movie.js
@@ -10,21 +10,18 @@ MovieApp.Views.MovieView = Backbone.View.extend({
 
   render: function(){
     console.log(this) //movie object
-    var poster = $.ajax({
-      type: 'GET',
-      url: this.model.attributes.Poster,
-      success: function(result){
-        i = new Image();
-        i.src = result;
-
-
-        console.log(result);
-      }
-    });
+    // var poster = $.ajax({
+    //   type: 'GET',
+    //   url: this.model.attributes.Poster,
+    //   // success: function(result){
+    //   //   console.log(result);
+    //   // }
+    // });
     // var parsed_data = JSON.parse(poster);
     // console.log(parsed_data)
     // console.log(poster);
     this.$el.html(this.model.attributes.Title+"("+this.model.attributes.Year+")" + this.model.attributes.Plot);
+    this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
     return this;
   }
 });
