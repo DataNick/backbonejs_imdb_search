9abac942c64e79679a78f18c14bd99134304c993
DataNick <DataNick@users.noreply.github.com>
2016-06-21 03:52:52 -0400

    Fixed styling by adding rows and columns

--- js/views/Movie.js
+++ js/views/Movie.js
@@ -9,7 +9,7 @@ MovieApp.Views.MovieView = Backbone.View.extend({
   },
 
   render: function(){
-    this.$el.html("<h1>" + this.model.attributes.Title + "</h1>" + "<h5>Released: "+this.model.attributes.Year+"</h5>" + "<div class = 'plot'>" + this.model.attributes.Plot) + "</div>" + this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
+    this.$el.html("<div class = 'row'><div class = 'col s5'><h1>" + this.model.attributes.Title + "</h1>" + "<h5>Released: "+this.model.attributes.Year+"</h5>" + this.model.attributes.Plot + "</div>" + "<img class = 'col s7' src=" + this.model.attributes.Poster + "/></div>");
     // this.$el.append("<div><img src=" + this.model.attributes.Poster + "/></div>");
     return this;
   }
